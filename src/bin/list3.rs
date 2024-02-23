c1d9b8aab2ab93d97fafa8b1300bcdd266216305
bekyiu <651731457@qq.com>
2024-02-23 21:55:28 +0800

    feat: list3

--- src/bin/list3.rs
+++ src/bin/list3.rs
@@ -53,13 +53,101 @@ impl List {
     }
 }
 
-
-fn main() {
-    let mut list = List::new();
-    list.push_left(1);
-    list.push_left(2);
-    list.push_left(3);
-    println!("{:?}", list);
-    list.pop_left();
-    println!("{:?}", list);
+// 实际上我们无需为List实现Drop trait
+// 原因是 Rust 自动为几乎所有类型都实现了 Drop
+// 例如我们自定义的结构体, 只要结构体的所有字段都实现了 Drop, 那结构体也会自动实现 Drop
+//
+// 但是, 有的时候这种自动实现可能不够优秀, 例如考虑以下链表:
+// list -> A -> B -> C
+// list被drop时, 要先drop A, 要drop A时 需要先drop B ..., 这是一个递归的过程
+// 所有如果链表太长, drop就会出问题
+//
+// 而我们可以手动为 List实现drop 来规避这个问题
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
+        while let Link::More(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
+            // boxed_node 在这里超出作用域并被 drop,
+            // 由于它的 `next` 字段拥有的 `Node` 被设置为 Link::Empty,
+            // 因此这里并不会发生递归drop
+        }
+    }
 }
+
+#[cfg(test)]
+mod test {
+    use std::mem;
+    use crate::{Link, Node};
+    use super::List;
+
+    #[test]
+    fn test_drop() {
+        let node2 = Node {
+            elem: 2,
+            next: Link::Empty,
+        };
+
+        let node1 = Node {
+            elem: 1,
+            next: Link::More(Box::new(node2)),
+        };
+
+        let mut list = List {
+            head: Link::More(Box::new(node1)),
+        };
+
+        println!("{:?}", list);
+
+        // cur_link一直位于栈上
+        let mut cur_link = mem::replace(&mut list.head, Link::Empty);
+        while let Link::More(mut boxed_node) = cur_link {
+            // boxed_node是栈上的指针, 指向堆上的Node结构
+            // 把boxed_node下一个元素的地址 赋值给了cur_link
+            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
+        }
+        // 此时cur_link是一个栈上的Link::Empty
+        println!("{:?}", cur_link);
+    }
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        // Check empty list behaves right
+        assert_eq!(list.pop_left(), None);
+
+        // Populate list
+        list.push_left(1);
+        list.push_left(2);
+        list.push_left(3);
+
+        // Check normal removal
+        assert_eq!(list.pop_left(), Some(3));
+        assert_eq!(list.pop_left(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push_left(4);
+        list.push_left(5);
+
+        // Check normal removal
+        assert_eq!(list.pop_left(), Some(5));
+        assert_eq!(list.pop_left(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop_left(), Some(1));
+        assert_eq!(list.pop_left(), None);
+    }
+
+    // 如果是默认的Drop实现, 这个测试是无法通过的
+    #[test]
+    fn long_list() {
+        let mut list = List::new();
+        for i in 0..100000 {
+            list.push_left(i);
+        }
+        drop(list);
+    }
+}
+
+fn main() {}
